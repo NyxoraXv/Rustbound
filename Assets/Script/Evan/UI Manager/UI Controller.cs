96f0134dfe27f56d12fdac3d6d021c4866dcebd0
NyxoraXv <87947067+NyxoraXv@users.noreply.github.com>
2024-10-09 00:24:20 +0700

    weapon shop anim

--- Assets/Script/Evan/UI Manager/UI Controller.cs
+++ Assets/Script/Evan/UI Manager/UI Controller.cs
@@ -1,10 +1,14 @@
 using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField] private Volume globalVolume;
+    private Vignette vignette;
+    private DepthOfField depthOfField;
     private int currentState;
     public GameObject HUD, WeaponMarket, TurretMarket, Inventory, PlacementUI;
 
@@ -13,9 +17,48 @@ public class UIController : MonoBehaviour
         HUDSetactive(false);
 
         DOVirtual.DelayedCall(5f, () => { HUDSetactive(true); });
+        globalVolume.profile.TryGet<Vignette>(out vignette);
+        globalVolume.profile.TryGet<DepthOfField>(out depthOfField);
     }
 
-    public void HUDSetactive(bool setActive) 
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.O)) {
+        setUIState(currentState+1);
+        }
+    }
+
+    public void triggerGlobalVolume(bool setActive)
+    {
+        if (setActive)
+        {
+            StartCoroutine(TweenGlobalVolume(0.5f, 20f, 1f)); // Tween to 0.5 and 20 over 1 second
+        }
+        else
+        {
+            StartCoroutine(TweenGlobalVolume(0f, 15f, 1f)); // Tween to 0 and 15 over 1 second
+        }
+    }
+
+    private IEnumerator TweenGlobalVolume(float targetVignetteIntensity, float targetFocusDistance, float duration)
+    {
+        float startVignetteIntensity = vignette.intensity.value;
+        float startFocusDistance = depthOfField.focusDistance.value;
+
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / duration;
+            vignette.intensity.value = Mathf.Lerp(startVignetteIntensity, targetVignetteIntensity, normalizedTime);
+            depthOfField.focusDistance.value = Mathf.Lerp(startFocusDistance, targetFocusDistance, normalizedTime);
+            yield return null;
+        }
+
+        // Ensure we set to final value
+        vignette.intensity.value = targetVignetteIntensity;
+        depthOfField.focusDistance.value = targetFocusDistance;
+    }
+
+    public void HUDSetactive(bool setActive)
     {
         if (!setActive)
         {
@@ -38,17 +81,23 @@ public class UIController : MonoBehaviour
     {
         if (!setActive)
         {
-
+            // Deactivate WeaponMarket
         }
         else
         {
-
+            // Activate WeaponMarket
         }
     }
 
     public void setUIState(int stateID)
     {
         currentState = stateID;
+
+        // Deactivate global volume if outside states 1, 2, or 3
+        if (stateID < 1 || stateID > 3)
+        {
+            triggerGlobalVolume(false);
+        }
 
         switch (currentState)
         {
@@ -60,6 +109,7 @@ public class UIController : MonoBehaviour
                 PlacementUI.SetActive(false);
                 break;
             case 1:
+                triggerGlobalVolume(true);  // Trigger for state 1
                 HUD.SetActive(false);
                 WeaponMarket.SetActive(true);
                 TurretMarket.SetActive(false);
@@ -67,6 +117,7 @@ public class UIController : MonoBehaviour
                 PlacementUI.SetActive(false);
                 break;
             case 2:
+                triggerGlobalVolume(true);  // Trigger for state 2
                 HUD.SetActive(false);
                 WeaponMarket.SetActive(false);
                 TurretMarket.SetActive(true);
@@ -74,6 +125,7 @@ public class UIController : MonoBehaviour
                 PlacementUI.SetActive(false);
                 break;
             case 3:
+                triggerGlobalVolume(true);  // Trigger for state 3
                 HUD.SetActive(false);
                 WeaponMarket.SetActive(false);
                 TurretMarket.SetActive(false);
@@ -96,4 +148,5 @@ public class UIController : MonoBehaviour
                 break;
         }
     }
+
 }
--- Assets/Script/Evan/Weapon Manager/WeaponHoverEffect.cs
+++ Assets/Script/Evan/Weapon Manager/WeaponHoverEffect.cs
@@ -6,15 +6,23 @@ using UnityEngine.UI;
 public class WeaponHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image weaponImage, decoratorImage;
-    [SerializeField] private GameObject statsLine;
+    [SerializeField] private GameObject statsLine, BuyButton;
     [SerializeField] private Vector3 hoverOffset = new Vector3(0f, 10f, 0f); // Set your desired offset here
+    [SerializeField] private float buttonHoverScale = 1.1f; // Scale up value for hover effect
+    [SerializeField] private float buttonClickScale = 0.9f; // Scale down value for click effect
+    [SerializeField] private float duration = 0.3f; // Duration for the animations
 
     private Vector3 originalPosition; // Store the original position
+    private Button buyButtonComponent;
 
     private void Start()
     {
         // Store the initial position of the weaponImage
         originalPosition = weaponImage.rectTransform.localPosition;
+
+        // Add the click listener to the BuyButton
+        buyButtonComponent = BuyButton.GetComponent<Button>();
+        buyButtonComponent.onClick.AddListener(OnBuyButtonClick);
     }
 
     // This method will be called when the pointer enters the UI element
@@ -34,7 +42,7 @@ public class WeaponHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerEx
         // Scale up and move the weaponImage with the specified offset
         weaponImage.rectTransform.DOScale(1.1f, 0.5f); // Scale up
         weaponImage.rectTransform.DOLocalMove(originalPosition + hoverOffset, 0.5f); // Move to the original position plus offset
-        statsLine.transform.DOLocalMoveX(originalPosition.z+hoverOffset.z, 0.5f);
+        statsLine.transform.DOScale(0.9f, 0.5f);
     }
 
     public void OffHover()
@@ -42,6 +50,29 @@ public class WeaponHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerEx
         // Scale down and return to the original position
         weaponImage.rectTransform.DOScale(1f, 0.5f); // Scale down effect
         weaponImage.rectTransform.DOLocalMove(originalPosition, 0.5f); // Return to original position
-        statsLine.transform.DOLocalMoveX(originalPosition.z, 0.5f);
+        statsLine.transform.DOScale(1f, 0.5f);
+    }
+
+    // Method for BuyButton hover effect
+    public void OnBuyButtonHoverEnter()
+    {
+        // Scale up the BuyButton on hover
+        BuyButton.transform.DOScale(1.1f, 0.4f);
+    }
+
+    public void OnBuyButtonHoverExit()
+    {
+        // Scale back to original size when not hovering
+        BuyButton.transform.DOScale(1f, duration);
+    }
+
+    // Method for BuyButton click effect
+    private void OnBuyButtonClick()
+    {
+        // Scale down and then scale up for a click bounce effect
+        BuyButton.transform.DOScale(buttonClickScale, duration / 2).OnComplete(() =>
+        {
+            BuyButton.transform.DOScale(1f, duration / 2);
+        });
     }
 }
