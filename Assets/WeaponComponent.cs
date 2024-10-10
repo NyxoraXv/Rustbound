ae6295b60692efd6003e2fbba5ce119086b0d8cc
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-10-10 10:17:29 +0700

    add enemy

--- Assets/WeaponComponent.cs
+++ Assets/WeaponComponent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WeaponComponent : MonoBehaviour
 {
     [SerializeField] private float damage = 5f;
-    [SerializeField] private float delay = 0.1f;
+    // [SerializeField] private float delay = 0.1f;
     void Start()
     {
         
